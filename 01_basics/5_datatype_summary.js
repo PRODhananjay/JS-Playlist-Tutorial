3372cacc84def35f5508fdf38e4cbb6c397b904e
DHANANJAY <57885767+PRODhananjay@users.noreply.github.com>
2024-11-23 06:05:00 +0000

    done

---
+++ 01_basics/5_datatype_summary.js
@@ -0,0 +1,60 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const num1 = 1234
+// console.log(typeof num1); // Output: "number" 
+
+// const bigIntValue = 12345678901234567890n;
+// console.log(typeof bigIntValue); // Output: "bigint"
+
+// const stringx = "dhananjay"
+// console.log(typeof stringx); // Output: "string" 
+
+// const bool = false
+// console.log(typeof bool); // Output: "boolean" 
+ 
+// let x;
+// console.log(x); // Output: "undefined" 
+
+// console.log(typeof null); // Output: "object" 
+
+// const mySymbol = Symbol("my description"); 
+// console.log(typeof mySymbol); // Output: "symbol"
+
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// console.log(typeof anotherId);
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
