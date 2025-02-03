6b347e157d1590d157022a22839c8c50fec06482
DHANANJAY <57885767+PRODhananjay@users.noreply.github.com>
2025-02-03 10:42:13 +0000

    done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,90 @@
+// const arr = [10 , 20 , 33 ,6, 44]
+// console.log(arr);
+// console.log(arr[2]);
+
+// const myHeros = ["Iron Man", " Super Man"]
+// console.log(myHeros);
+
+// const cricket = new Array ("Rohit Sharma", " Virat Kohli");
+// console.log(typeof cricket);
+
+// ----------------------- Array Methods ------------------------
+
+const methodArray = [10, 20 , 30 , 40 , 50 , 60 , 70, 600]
+// console.log(methodArray);
+
+// methodArray.push(80)
+// console.log(methodArray);
+
+// methodArray.pop()
+// console.log(methodArray);
+
+// methodArray.unshift(9)
+// console.log(methodArray);
+
+// methodArray.shift()
+// console.log(methodArray);
+
+// methodArray.includes(40)
+// console.log(methodArray);
+
+// methodArray.includes((40))
+// console.log(methodArray.includes(40));
+
+
+ // slice, splice
+const myArr = [10,20,30,40,50,60]
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
+
+
+
+// // array
+
+// const myArr = [0, 1, 2, 3, 4, 5]
+// const myHeors = ["shaktiman", "naagraj"]
+
+// const myArr2 = new Array(1, 2, 3, 4)
+// // console.log(myArr[1]);
+
+// // Array methods
+
+// // myArr.push(6)
+// // myArr.push(7)
+// // myArr.pop()
+
+// // myArr.unshift(9)
+// // myArr.shift()
+
+// // console.log(myArr.includes(9));
+// // console.log(myArr.indexOf(3));
+
+// // const newArr = myArr.join()
+
+// // console.log(myArr);
+// // console.log( newArr);
+
+
+// // slice, splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
